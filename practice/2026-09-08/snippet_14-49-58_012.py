0b56cdc95496230c641f6bfd54fb406ebc1a0803
avanishkasar <avanishkasar@gmail.com>
2026-09-08 14:49:58 +0000

    practice: went over list vs set: when to use which

---
+++ practice/2026-09-08/snippet_14-49-58_012.py
@@ -0,0 +1,8 @@
+# Practice commit 12
+# Topic: went over list vs set: when to use which
+
+stk = []
+for ch in '({[]})':
+    if ch in '({[': stk.append(ch)
+    else: stk.pop() if stk else None
+print(not stk)
