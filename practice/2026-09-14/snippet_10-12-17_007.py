b3dec2f821f3b1beaaf4f2974bfa524864d64761
avanishkasar <151715253+avanishkasar@users.noreply.github.com>
2026-09-14 10:12:17 +0000

    practice: practiced set operations: union intersection difference

---
+++ practice/2026-09-14/snippet_10-12-17_007.py
@@ -0,0 +1,6 @@
+# Practice commit 7
+# Topic: practiced set operations: union intersection difference
+
+n = 17
+is_prime = n > 1 and all(n % i != 0 for i in range(2, int(n**0.5)+1))
+print(is_prime)
